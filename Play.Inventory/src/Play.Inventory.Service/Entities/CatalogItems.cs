87f31f1f684e191fb9b146750aa0c57f0e547340
BogdanH95 <bogdan.halmaghi@gmail.com>
2025-01-08 13:26:23 +0200

    Add Catalog.Contracts and bump version for Common

--- Play.Inventory/src/Play.Inventory.Service/Entities/CatalogItems.cs
+++ Play.Inventory/src/Play.Inventory.Service/Entities/CatalogItems.cs
@@ -5,7 +5,6 @@ namespace Play.Inventory.Service.Entities
     public class CatalogItem : IEntity
     {
         public Guid Id { get; set; }
-
         public required string Name { get; set; }
         public required string Description { get; set; }
     }
